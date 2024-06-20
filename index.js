545c33dc443f02d6f69fb521d4f90d822492afa2
John Doe <g_anz@mail.ru>
2024-06-20 11:06:09 +0300

    333

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-import data from "./respons.json" assert { type: "json" };
+import { readFile } from 'fs/promises';
+const json = JSON.parse(
+  await readFile(
+    new URL('./respons.json', import.meta.url)
+  )
+);
 
-console.log(data);
+let groups = [];
+
+json.map((i) =>
+  i.data.map((j) =>
+    groups.push({
+      nameFamily: j.principal.nameFamily,
+      nameGiven: j.principal.nameGiven,
+      nameMiddle: j.principal.nameMiddle,
+    })
+  )
+)
+
+console.log(groups);
